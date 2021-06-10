68f5665a13923136e76d71f89c46482ee165d0a3
jismol <jismol2007@gmail.com>
2021-06-10 11:13:06 +0530

    10/6/2021 javascript starting

---
+++ Javascript programs/javascriptsecondfile.js
@@ -0,0 +1,25 @@
+console.log("Have a nice day");
+//to print some thing we use console.log
+
+/*comment- single line //
+multiline comment /* -----and  ----------- */
+//variable - to represent memory location
+//javascript variable declaration
+//var,let,const
+var company_name = "Luminal"
+var location_name = "Kakkanad"
+console.log(company_name+location_name)
+console.log("our company"+company_name+"located in"+location_name)
+var p_name = "jismol"
+var age = "35"
+console.log("Hi I am "+p_name,age+" years old")
+var state = "Kerala"
+var act_cases = 20000
+var death = 1000
+var cured = 15000
+console.log(`in ${state} ${act_cases} and ${cured} and ${death} reported`)
+typeof act_cases
+
+console.log(typeof act_cases);
+is_active = true;
+console.log(typeof(is_active));
---
+++ Javascript programs/swapping.js
@@ -0,0 +1,6 @@
+var num1=10 , num2 = 20
+console.log(`value b4 swaping num1= ${num1} num2= ${num2} `);
+var num3 = num2
+num2 = num1
+num1 = num3
+console.log(`value after swaping num1= ${num1} num2= ${num2} `);
