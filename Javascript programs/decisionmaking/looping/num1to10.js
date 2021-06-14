17a67fd32c233b07bff312019e40945df0e78cb5
Jismol <jismol2007@gmail.com>
2021-06-14 17:23:20 +0530

    looping javascript backup

---
+++ Javascript programs/decisionmaking/looping/num1to10.js
@@ -0,0 +1,8 @@
+var i=1;
+var sum=0;
+while(i<=10){
+    var sum=sum+i;
+    // console.log(i);
+    i++;
+}
+console.log(sum);
--- Javascript programs/decisionmaking/seclargest.js
+++ Javascript programs/decisionmaking/seclargest.js
@@ -5,4 +5,24 @@ if (num1>num2 & num1>num3)
     {
         console.log(`second largest number is ${num2}`);
     }
+    else{
+        console.log(`second largest number is ${numb3}`);
+    }
 }
+if (num2>=num1 & num2>=num3)
+{
+    if(num1>=num3)
+    {
+        console.log(`second largest number is ${num1}`);
+    }
+    else{
+        console.log(`second largest number is ${num3}`);
+    }
+}
+if (num1>=num2){
+    console.log(`second largest number is ${num1}`);
+}
+    else
+    {
+console.log(`second largest number is ${num2}`);
+    }
