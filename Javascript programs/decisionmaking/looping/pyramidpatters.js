1b8594c104e51c451ed3731479c8c2f88ff4f202
Jismol <jismol2007@gmail.com>
2021-06-16 15:42:38 +0530

    pyrmidpatterns

--- Javascript programs/decisionmaking/looping/pyramidpatters.js
+++ Javascript programs/decisionmaking/looping/pyramidpatters.js
@@ -19,16 +19,58 @@
 //     space="";
     
 // }
-for (let row=1;row<=5;row++){
-    let str=""
-    for(let space=5-row;space>=1;space--)
+// for (let row=1;row<=5;row++){
+//     let str=""
+//     for(let space=5-row;space>=1;space--)
+//     {
+//         str+=" "
+//     }
+//         for (let star=1;star<=row;star++)
+//         {
+//             str+="* "
+//         }
+    
+//     console.log(str);
+// // }
+// var number=9
+// for (let row=1;row<=number;row++)
+// {
+//     let str="";
+//     for(let space=number-row;space>=1;space--)
+//     {
+//         str+=" "
+//     }
+//     for (let star=1;star<=row;star++)
+//     {
+//         str+="* "
+//     }
+//     console.log(str);
+// }
+// var row=9;
+// for(let i=row;i>=1;i--)
+// {
+//     str="";
+//     // for(let space=1;space<=row-i;row++)
+//     // {
+//     //     str+=" ";
+//     // }
+//     for(let j=1;j<=i;j++)
+//     {
+//         str+="* ";
+//     }
+//     console.log(str);
+// }
+var number=9;
+for (let row=number;row>=1;row--)
+{
+    let str="";
+    for(let space=1;space<=number-row;space++)//
     {
         str+=" "
     }
-        for (let star=1;star<=row;star++)
-        {
-            str+="* "
-        }
-    
+    for (let star=row;star>=1;star--)
+    {
+        str+="* "
+    }
     console.log(str);
 }
